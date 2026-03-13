fc0687eb857bb2769f0310fe4207bcff3d21d5e3
Matthew Lakatos <24mlaka@gmail.com>
2026-03-13 13:22:16 +0000

    Create logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,26 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from config import settings
+
+LOG_FILE = "pipeline.log"
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(settings.log_level)
+
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
+    file_handler = RotatingFileHandler(
+        LOG_FILE,
+        maxBytes=5_000_000,
+        backupCount=5
+    )
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
