c54e35efc70dfa9485a29f51ed3955b8bb4d0f86
Matthew Lakatos <24mlaka@gmail.com>
2026-03-14 11:17:09 +0000

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,1 +1,40 @@
+from url_queue import URLQueue
+from link_extractor import extract_links
+from crawl_rules import is_content_url
+from scraper import fetch_html
 
+
+async def crawl(seed_urls, max_pages=50, content_only=False):
+
+    queue = URLQueue()
+
+    queue.add_many(seed_urls)
+
+    discovered = []
+
+    while not queue.empty() and len(discovered) < max_pages:
+
+        url = queue.get()
+
+        try:
+
+            html = await fetch_html(url)
+
+            links = extract_links(html, url)
+
+            for link in links:
+
+                if content_only:
+
+                    if is_content_url(link):
+                        queue.add(link)
+
+                else:
+                    queue.add(link)
+
+        except Exception:
+            continue
+
+        discovered.append(url)
+
+    return discovered
