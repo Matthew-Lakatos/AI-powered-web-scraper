a5b9efe99b95d0ee4fa7b8204d45d5b5e29d7bcd
Matthew Lakatos <24mlaka@gmail.com>
2026-03-14 11:15:37 +0000

    Update crawl_rules.py

--- crawl_rules.py
+++ crawl_rules.py
@@ -1,1 +1,14 @@
+CONTENT_PATTERNS = [
+    "article",
+    "news",
+    "blog",
+    "story",
+    "post"
+]
 
+
+def is_content_url(url):
+
+    url = url.lower()
+
+    return any(p in url for p in CONTENT_PATTERNS)
