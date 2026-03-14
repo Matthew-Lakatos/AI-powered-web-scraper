5635a38a7531061e471ef1f966b98ba89021ada7
Matthew Lakatos <24mlaka@gmail.com>
2026-03-14 11:08:42 +0000

    Update url_queue.py

--- url_queue.py
+++ url_queue.py
@@ -1,1 +1,30 @@
+from collections import deque
 
+
+class URLQueue:
+
+    def __init__(self):
+        self.queue = deque()
+        self.seen = set()
+
+    def add(self, url):
+
+        if url not in self.seen:
+            self.queue.append(url)
+            self.seen.add(url)
+
+    def add_many(self, urls):
+
+        for url in urls:
+            self.add(url)
+
+    def get(self):
+
+        if self.queue:
+            return self.queue.popleft()
+
+        return None
+
+    def empty(self):
+
+        return len(self.queue) == 0
