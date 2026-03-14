ee2ae6c3b9ac5c4ab231c1d51b30ec22124f6c66
Matthew Lakatos <24mlaka@gmail.com>
2026-03-14 11:15:03 +0000

    Update link_extractor.py

--- link_extractor.py
+++ link_extractor.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+
+def extract_links(html, base_url):
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    links = []
+
+    for tag in soup.find_all("a", href=True):
+
+        link = urljoin(base_url, tag["href"])
+
+        if link.startswith("http"):
+            links.append(link)
+
+    return links
