6649eaeeb2322724983fc5a2b3619e17966a32d8
Matthew Lakatos <24mlaka@gmail.com>
2026-03-14 17:26:25 +0000

    Update narrative_engine.py

--- narrative_engine.py
+++ narrative_engine.py
@@ -1,5 +1,8 @@
 import numpy as np
 from embeddings import generate_embedding
+import time
+import math
+
 
 SIM_THRESHOLD = 0.82
 
@@ -15,6 +18,23 @@ class NarrativeEngine:
         b = np.array(b)
 
         return float(np.dot(a, b))
+
+    def recency_weight(timestamp):
+
+        age_days = (time.time() - timestamp) / 86400
+    
+        return math.exp(-0.05 * age_days)
+
+
+    def narrative_importance(cluster):
+    
+        credibility = cluster.get("credibility", 0.5)
+    
+        timestamp = cluster.get("timestamp", time.time())
+    
+        size = cluster.get("size", 1)
+    
+        return credibility * recency_weight(timestamp) * size
 
     def add_article(self, text, summary):
 
